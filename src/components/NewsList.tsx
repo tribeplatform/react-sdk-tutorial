98e595cf0f05ee2802addf0858d51cb77c659b50
Keyvan <keyvankhademi@gmail.com>
2022-01-21 14:53:53 -0800

    Upvote

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {useFeed} from "@tribeplatform/react-sdk/hooks";
 import {simplifyPaginatedResult} from "@tribeplatform/react-sdk/utils";
 import {Post} from "@tribeplatform/gql-client/types";
 import {Link} from "react-router-dom";
+import {Upvote} from "./Upvote";
+import {useFeed} from "@tribeplatform/react-sdk/hooks";
 
 function NewsList() {
     const {data} = useFeed({
-        fields: 'basic',
+        fields: {
+            reactions: {
+                fields: 'all',
+                variables: {
+                    limit: 100,
+                }
+            }
+        },
         variables: {
             limit: 100,
         }
@@ -16,15 +24,21 @@ function NewsList() {
     return (
         <div className="w-3/4 m-auto flex flex-col gap-5">
             {posts.map((post, i) => (
-                <div className="flex gap-2 bg-gray-100 rounded-lg p-2">
-                    <div>
+                <div className="flex gap-2 bg-gray-100 rounded-lg p-2" key={post?.id}>
+                    <div className="flex flex-col justify-center">
                         {i + 1}.
                     </div>
-                    <Link to={`/${post.id}`}>
-                        <div>
-                            {post.title}
+                    <Upvote post={post}/>
+                    <div className="flex flex-col">
+                        <Link to={`/${post.id}`}>
+                            <div>
+                                {post.title}
+                            </div>
+                        </Link>
+                        <div className="text-xs text-gray-500">
+                            {post.reactionsCount} upvotes
                         </div>
-                    </Link>
+                    </div>
                 </div>
             ))}
         </div>
