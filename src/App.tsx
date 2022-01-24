c850b5594ae6d819d015e76b0f24473dc5f302e3
Keyvan <keyvankhademi@gmail.com>
2022-01-24 09:00:17 -0800

    Create News

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Routes, Route, Link} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import NewsList from "./components/NewsList";
 import {NewsPage} from "./components/NewsPage";
+import {Composer} from "./components/Composer";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<NewsList/>}/>
-            <Route path="/:postId" element={<NewsPage />} />
+            <Route path="/submit" element={<Composer/>}/>
+            <Route path="/:postId" element={<NewsPage/>}/>
         </Routes>
     );
 }
