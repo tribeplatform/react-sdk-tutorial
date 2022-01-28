530392b8eb4da75899a8c813ff895a7313d89bcc
Keyvan <keyvankhademi@gmail.com>
2022-01-28 15:40:18 -0800

    2. News Page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import NewsList from "./components/NewsList";
+import {NewsPage} from "./components/NewsPage";
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<NewsList/>}/>
+            <Route path="/:postId" element={<NewsPage/>}/>
+        </Routes>
+    );
+}
+
+export default App;
---
+++ src/typings.d.ts
@@ -0,0 +1,3 @@
+declare module "react-infinite-scroller" {
+    export default any;
+}
